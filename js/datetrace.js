ae1bea71841d6d66a7a2279fc23d398a7dc836c1
StonePick <kstonire@gmail.com>
2025-09-29 13:48:47 +0800

    Site updated: 2025-09-29 13:48:46

---
+++ js/datetrace.js
@@ -0,0 +1,17 @@
+(function() {
+    var times = document.getElementsByTagName('time');
+    if (times.length === 0) { return; }
+    var posts = document.getElementsByClassName('post-content');
+    if (posts.length === 0) { return; }
+  
+    var pubTime = new Date(times[0].dateTime);  /* 文章发布时间戳 */
+    var now = Date.now()  /* 当前时间戳 */
+    var interval = parseInt(now - pubTime)
+    /* 发布时间超过指定时间（毫秒） */
+    if (interval > 3600*24*180*1000){
+      var days = parseInt(interval / 86400000) /* 节点：180天 */
+      posts[0].innerHTML = '<div class="note note-warning" style="font-size:0.9rem"><p>' +
+        '<div class="h6">🕗Timeliness Reminder</div><p>This is an article posted ' + days + ' ago, in which the content might have been outdated.' +
+        '</p></p></div>' + posts[0].innerHTML;
+    }
+  })();
